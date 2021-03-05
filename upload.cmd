f51713f98a27f8a2f8422c46d1eb3ccd06f796b7
Creammmmm <49014295+Creammmmm@users.noreply.github.com>
2021-03-05 16:28:14 +0800

    Add files via upload

--- upload.cmd
+++ upload.cmd
@@ -1,10 +1,38 @@
 @echo off
+if "%1"=="hide" goto CmdBegin
+start mshta vbscript:createobject("wscript.shell").run("""%~0"" hide",0)(window.close)&&exit
+:CmdBegin
 title upload
+tasklist /v /fo csv | findstr /i "upload" >> log.txt
 :loop
+
 set d=%date:~0,10%
 set t=%time:~0,8%
-echo %d% %t%
-ping -n 18037 127.1>nul
-call python upload.py -u 11111111 -p 11111111 -f data/upload.txt
+set currentTime=%time:~0,2%
+
+if %currentTime%==7 (
+	echo/ >> log.txt
+	call python upload.py -u 111111 -p 111111 -f data/upload.txt >> log.txt
+	echo %d% %t% >> log.txt
+	ping -n 18000 127.1>nul
+	)
+
+if %currentTime%==12 (
+	echo/ >> log.txt
+	call python upload.py -u 111111 -p 111111 -f data/upload.txt >> log.txt
+	echo %d% %t% >> log.txt
+	ping -n 18000 127.1>nul
+	)
+
+if %currentTime%==18 (
+	echo/ >> log.txt
+	call python upload.py -u 111111 -p 111111 -f data/upload.txt >> log.txt
+	echo %d% %t% >> log.txt
+	ping -n 36000 127.1>nul
+	)
+
+echo %d% %t% >> log.txt
+ping -n 618 127.1>nul
+
 goto loop
 pause
